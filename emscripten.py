b35b65c7487282da196ca898f63f50798a4c896a
Bruce Mitchener <bruce.mitchener@gmail.com>
2013-04-06 02:15:42 +0700

    Hack to avoid pulling in tools/shared.py
    Right now, if we pull in tools/shared.py, we need an ~/.emscripten
    file which breaks the build bot.
    
    We specify -v which avoids needing config here, but elsewhere when
    the DEBUG_CACHE flag is on, we need config, so this avoids that.

--- emscripten.py
+++ emscripten.py
@@ -711,7 +711,7 @@ WARNING: You should normally never use this! Use emcc instead.
     DEBUG_CACHE = get_configuration().DEBUG_CACHE
   else:
     DEBUG = keywords.verbose
-    DEBUG_CACHE = keywords.verbose
+    DEBUG_CACHE = False # keywords.verbose # FIX ME SOME DAY
 
   cache = cache_module.Cache()
   temp_files.run_and_clean(lambda: main(
