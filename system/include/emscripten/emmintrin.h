d4debcf7bef8db38aed6300295570cc5c8fadac8
Jukka Jylänki <jujjyl@gmail.com>
2014-11-20 18:45:41 -0500

    Add SSE2 __m128d, _mm_shuffle_epi32() and _mm_move_sd().

--- system/include/emscripten/emmintrin.h
+++ system/include/emscripten/emmintrin.h
@@ -1,6 +1,9 @@
+#pragma once
+
 #include <xmmintrin.h>
 
 typedef int32x4 __m128i;
+typedef float64x2 __m128d;
 
 static __inline__ __m128i __attribute__((__always_inline__))
 _mm_set_epi32(int z, int y, int x, int w)
@@ -62,6 +65,12 @@ _mm_sub_epi32(__m128i a, __m128i b)
   return a - b;
 }
 
+#define _mm_shuffle_epi32(a, imm) __extension__ ({ \
+  __m128i __a = (a); \
+  (__m128i)__builtin_shufflevector((__v4si)__a, (__v4si) _mm_set1_epi32(0), \
+                                   (imm) & 0x3, ((imm) & 0xc) >> 2, \
+                                   ((imm) & 0x30) >> 4, ((imm) & 0xc0) >> 6); })
+
 static __inline__ __m128 __attribute__((__always_inline__))
 _mm_castsi128_ps(__m128i a)
 {
@@ -84,4 +93,10 @@ static __inline__ __m128i __attribute__((__always_inline__))
 _mm_cvtps_epi32(__m128 a)
 {
   return emscripten_int32x4_fromFloat32x4(a);
+}
+
+static __inline__ __m128 __attribute__((__always_inline__))
+_mm_move_sd(__m128d __a, __m128d __b)
+{
+  return __builtin_shufflevector(__a, __b, 2, 1);
 }
