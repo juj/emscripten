ae2bcde70afd6335bb0b93211863d4ceb98160b9
Jukka Jylänki <jujjyl@gmail.com>
2016-08-15 02:23:08 +0300

    Fix jsrun.py import

--- tools/jsrun.py
+++ tools/jsrun.py
@@ -8,7 +8,6 @@ if EM_PROFILE_TOOLCHAIN:
   check_call = profiled_check_call
   check_output = profiled_check_output
 else:
-  Popen = subprocess.Popen
   check_call = subprocess.check_call
   check_output = subprocess.check_output
 
