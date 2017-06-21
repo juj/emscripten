96100feff09f14a6ba7533beb12d3acf8172a14a
Jukka Jylänki <jujjyl@gmail.com>
2017-06-21 13:54:00 +0300

    Disable implicit swap when proxying for now, since it might lead to double swapping.

--- src/library_browser.js
+++ src/library_browser.js
@@ -1162,7 +1162,7 @@ var LibraryBrowser = {
       if (typeof GL !== 'undefined' && GL.currentContext) {
         if (GLctxIsOnParentThread) {
           // TODO: This should only be called if GL.currentContext.attributes.explicitSwapControl == false, but in a pthread, we don't currently know which value this flag has.
-          _emscripten_webgl_commit_frame();
+//          _emscripten_webgl_commit_frame();
         } else if (!GL.currentContext.attributes.explicitSwapControl && GL.currentContext.GLctx.commit) {
           GL.currentContext.GLctx.commit();
         }
