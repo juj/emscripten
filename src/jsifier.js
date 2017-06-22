787e3c94576c5b01a562c9d5d815739c497e8a5b
Jukka Jylanki <jujjyl@gmail.com>
2017-06-23 00:35:21 +0300

    Fix regex bug in proxying code insertion snippet.

--- src/jsifier.js
+++ src/jsifier.js
@@ -255,10 +255,10 @@ function JSify(data, functionsOnly) {
           var proxyingFunc = (proxyingMode === 'main' || proxyingMode === 'main_gl') ? '_emscripten_sync_run_in_browser_thread_' : '_emscripten_async_run_in_browser_thread_';
           if (sig.length > 1) {
             // If the function takes parameters, forward those to the proxied function call
-            snippet = snippet.replace(/function\s+(.*)?\s*\((.*?)\)\s*{/g, 'function $1($2) {\nif (' + proxyingCondition + ') return ' + proxyingFunc + sig + '(' + proxiedFunctionOrdinal++ + ', $2);');
+            snippet = snippet.replace(/function\s+(.*)?\s*\((.*?)\)\s*{/, 'function $1($2) {\nif (' + proxyingCondition + ') return ' + proxyingFunc + sig + '(' + proxiedFunctionOrdinal++ + ', $2);');
           } else {
             // No parameters to the function
-            snippet = snippet.replace(/function (.*)? {/g, 'function $1 {\nif (' + proxyingCondition + ') return ' + proxyingFunc + sig + '(' + proxiedFunctionOrdinal++ + ');');
+            snippet = snippet.replace(/function (.*)? {/, 'function $1 {\nif (' + proxyingCondition + ') return ' + proxyingFunc + sig + '(' + proxiedFunctionOrdinal++ + ');');
           }
           contentText = snippet;
           proxiedFunctionTable.push(finalName);
