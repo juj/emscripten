8e0aafc05e31d19ca2396d5eedbec4290e44d852
Jukka Jylanki <jujjyl@gmail.com>
2017-08-17 16:36:27 +0300

    Fix proxying of glBindAttribLocation, which needs to be synchronous instead of async.

--- src/library_gl.js
+++ src/library_gl.js
@@ -4147,7 +4147,7 @@ var LibraryGL = {
   },
 #endif
 
-  glBindAttribLocation__proxy: 'async_gl',
+  glBindAttribLocation__proxy: 'main_gl',
   glBindAttribLocation__sig: 'viii',
   glBindAttribLocation: function(program, index, name) {
 #if GL_ASSERTIONS
