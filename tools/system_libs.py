060a6dccd9b492cfa8209dc2720adba7df8f4e3b
Jukka Jylänki <jujjyl@gmail.com>
2017-08-26 15:36:35 +0300

    Rewrite EGL API as a C library to make it multithreading capable out of the box without needing to proxy.

--- tools/system_libs.py
+++ tools/system_libs.py
@@ -53,6 +53,7 @@ def calculate(temp_files, in_temp, stdout_, stderr_, forced=[]):
   libcxxabi_symbols = read_symbols(shared.path_from_root('system', 'lib', 'libcxxabi', 'symbols'))
   gl_symbols = read_symbols(shared.path_from_root('system', 'lib', 'gl.symbols'))
   al_symbols = read_symbols(shared.path_from_root('system', 'lib', 'al.symbols'))
+  egl_symbols = read_symbols(shared.path_from_root('system', 'lib', 'egl.symbols'))
   compiler_rt_symbols = read_symbols(shared.path_from_root('system', 'lib', 'compiler-rt.symbols'))
   pthreads_symbols = read_symbols(shared.path_from_root('system', 'lib', 'pthreads.symbols'))
   wasm_libc_symbols = read_symbols(shared.path_from_root('system', 'lib', 'wasm-libc.symbols'))
@@ -174,6 +175,10 @@ def calculate(temp_files, in_temp, stdout_, stderr_, forced=[]):
       ])
     pthreads_files += [os.path.join('pthread', 'library_pthread.c')]
     return build_libc(libname, pthreads_files, ['-O2', '-s', 'USE_PTHREADS=1'])
+
+  def create_egl(libname):
+    egl_files = [os.path.join('egl', 'library_egl.c')]
+    return build_libc(libname, egl_files, [])
 
   def create_wasm_libc(libname):
     # in asm.js we just use Math.sin etc., which is good for code size. But
@@ -434,6 +439,7 @@ def calculate(temp_files, in_temp, stdout_, stderr_, forced=[]):
                  ('al',            'bc', create_al,          al_symbols,          ['libc'],      False),
                  ('html5',         'bc', create_html5,       html5_symbols,       ['html5'],     False),
                  ('compiler-rt',   'a',  create_compiler_rt, compiler_rt_symbols, ['libc'],      False),
+                 ('egl',           'bc', create_egl,         egl_symbols,         ['libc'],      False),
                  (dlmalloc_name(), 'bc', create_dlmalloc,    [],                  [],            False)]
 
   if shared.Settings.USE_PTHREADS:
