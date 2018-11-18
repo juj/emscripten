be08c41f750fe72baa71cff79d19a5fe541f86a1
Jukka Jylänki <jujjyl@gmail.com>
2018-11-18 23:29:43 +0200

    Micro-optimize UTF8ToString to refer to long String.fromCharCode() fewer times.

--- src/preamble_minimal.js
+++ src/preamble_minimal.js
@@ -159,23 +159,27 @@ function UTF8ArrayToString(u8Array, idx) {
       // https://tools.ietf.org/html/rfc3629
       u0 = u8Array[idx++];
       if (!u0) return str;
-      if (!(u0 & 0x80)) { str += String.fromCharCode(u0); continue; }
-      u1 = u8Array[idx++] & 63;
-      if ((u0 & 0xE0) == 0xC0) { str += String.fromCharCode(((u0 & 31) << 6) | u1); continue; }
-      u2 = u8Array[idx++] & 63;
-      if ((u0 & 0xF0) == 0xE0) {
-        u0 = ((u0 & 15) << 12) | (u1 << 6) | u2;
-      } else {
-        u3 = u8Array[idx++] & 63;
-        if ((u0 & 0xF8) == 0xF0) {
-          u0 = ((u0 & 7) << 18) | (u1 << 12) | (u2 << 6) | u3;
+      if ((u0 & 0x80)) {
+        u1 = u8Array[idx++] & 63;
+        if ((u0 & 0xE0) == 0xC0) {
+          u0 = ((u0 & 31) << 6) | u1;
         } else {
-          u4 = u8Array[idx++] & 63;
-          if ((u0 & 0xFC) == 0xF8) {
-            u0 = ((u0 & 3) << 24) | (u1 << 18) | (u2 << 12) | (u3 << 6) | u4;
+          u2 = u8Array[idx++] & 63;
+          if ((u0 & 0xF0) == 0xE0) {
+            u0 = ((u0 & 15) << 12) | (u1 << 6) | u2;
           } else {
-            u5 = u8Array[idx++] & 63;
-            u0 = ((u0 & 1) << 30) | (u1 << 24) | (u2 << 18) | (u3 << 12) | (u4 << 6) | u5;
+            u3 = u8Array[idx++] & 63;
+            if ((u0 & 0xF8) == 0xF0) {
+              u0 = ((u0 & 7) << 18) | (u1 << 12) | (u2 << 6) | u3;
+            } else {
+              u4 = u8Array[idx++] & 63;
+              if ((u0 & 0xFC) == 0xF8) {
+                u0 = ((u0 & 3) << 24) | (u1 << 18) | (u2 << 12) | (u3 << 6) | u4;
+              } else {
+                u5 = u8Array[idx++] & 63;
+                u0 = ((u0 & 1) << 30) | (u1 << 24) | (u2 << 18) | (u3 << 12) | (u4 << 6) | u5;
+              }
+            }
           }
         }
       }
