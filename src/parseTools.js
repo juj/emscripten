12bcfdcd9ea9dbeb140feba54b3c8de307c32151
Jukka Jylänki <jujjyl@gmail.com>
2019-02-08 20:05:17 +0200

    Add exceptions support to MINIMAL_RUNTIME.

--- src/parseTools.js
+++ src/parseTools.js
@@ -1057,7 +1057,7 @@ function exportedAsmFunc(func) {
   if (!MAIN_MODULE && !SIDE_MODULE) {
     return func;
   } else {
-    return "Module['" + func + "]";
+    return "Module['" + func + "']";
   }
 }
 
