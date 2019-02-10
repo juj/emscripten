b9c26142e3d6fa8b873585a423946dd5f6d1a191
Jukka Jylänki <jujjyl@gmail.com>
2019-02-10 20:31:59 +0200

    Minify the generated HTML file, if building with -o out.html. Add option -s MINIFY_HTML=0 to allow disabling the minification, in case it is problematic.

--- tests/test_other.py
+++ tests/test_other.py
@@ -8910,10 +8910,10 @@ int main () {
     hello_webgl_sources = [path_from_root('tests', 'minimal_webgl', 'main.cpp'), path_from_root('tests', 'minimal_webgl', 'webgl.c'), '--js-library', path_from_root('tests', 'minimal_webgl', 'library_js.js'), '-s', 'RUNTIME_FUNCS_TO_IMPORT=[]', '-s', 'USES_DYNAMIC_ALLOC=2', '-lGL']
 
     test_cases = [
-      (asmjs + opts, hello_world_sources, {'a.html': 665, 'a.js': 289, 'a.asm.js': 113, 'a.mem': 6}),
-      (opts, hello_world_sources, {'a.html': 623, 'a.js': 624, 'a.wasm': 86}),
-      (asmjs + opts, hello_webgl_sources, {'a.html': 665, 'a.js': 5249, 'a.asm.js': 10965, 'a.mem': 321}),
-      (opts, hello_webgl_sources, {'a.html': 623, 'a.js': 5313, 'a.wasm': 8978})
+      (asmjs + opts, hello_world_sources, {'a.html': 394, 'a.js': 289, 'a.asm.js': 113, 'a.mem': 6}),
+      (opts, hello_world_sources, {'a.html': 370, 'a.js': 624, 'a.wasm': 86}),
+      (asmjs + opts, hello_webgl_sources, {'a.html': 394, 'a.js': 5249, 'a.asm.js': 10965, 'a.mem': 321}),
+      (opts, hello_webgl_sources, {'a.html': 370, 'a.js': 5313, 'a.wasm': 8978})
     ]
 
     success = True
