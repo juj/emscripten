f0bc55fe4ac69cfaa72fdb4a6f9c45a2a6f4f08b
Jukka Jylänki <jujjyl@gmail.com>
2025-08-21 20:06:02 +0300

    Xzxcf

--- test/runner.py
+++ test/runner.py
@@ -597,6 +597,11 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
       logger.warning('KeyboardInterrupt')
       sys.exit(1)
+    except BlockingIOError:
+      fd = sys.stderr.fileno()
+      flags = fcntl.fcntl(fd, fcntl.F_GETFL)
+      if flags & ~os.O_NONBLOCK:
+        print('STDER WASS NONBLOCK')
 else:
   # We are not the main process, and most likely a child process of
   # the multiprocess pool.  In this mode the modifications made to the
