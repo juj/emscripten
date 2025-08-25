17870e8814fdcab89f535d0620d666063f3df15e
Slavomir Kucera <slavomir.kucera@broadcom.com>
2025-08-25 12:02:19 +0200

    add tests

--- test/test_other.py
+++ test/test_other.py
@@ -2965,6 +2965,7 @@ More info: https://emscripten.org
     'safeHeap': (['safeHeap'],),
     'object-literals': ([],),
     'LittleEndianHeap': (['littleEndianHeap'],),
+    'LittleEndianGrowableHeap': (['growableHeap','littleEndianHeap'],),
   })
   @crossplatform
   def test_js_optimizer(self, passes, filename=None):
