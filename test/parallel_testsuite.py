69181ddbf92136e8842f2f2145d519fd150edc83
Jukka Jylänki <jujjyl@gmail.com>
2025-09-11 02:19:53 +0300

    common.

--- test/parallel_testsuite.py
+++ test/parallel_testsuite.py
@@ -113,8 +113,8 @@ class ParallelTestSuite(unittest.BaseTestSuite):
 
     # Remove any old test files before starting the run
     utils.delete_file(common.flaky_tests_log_filename)
-    utils.delete_file(browser_spawn_lock_filename)
-    utils.delete_file(f'{browser_spawn_lock_filename}_counter')
+    utils.delete_file(common.browser_spawn_lock_filename)
+    utils.delete_file(f'{common.browser_spawn_lock_filename}_counter')
 
     # If we are running with --failing-and-slow-first, then the test list has been
     # pre-sorted based on previous test run results. Otherwise run the tests in
