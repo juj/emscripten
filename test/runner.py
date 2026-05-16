4cbf041dbbeedc1083c5c9b4e210b2de1f798927
Jukka Jylänki <jujjyl@gmail.com>
2026-05-16 23:34:03 +0300

    asdf

--- test/runner.py
+++ test/runner.py
@@ -2,6 +2,12 @@ import sys
 
 print("stdout encoding:", sys.stdout.encoding)
 print("stderr encoding:", sys.stderr.encoding)
+
+try:
+  print('A ä☃ö Z.cpp')
+except Exception as e:
+  print(str(e))
+
 sys.stdout.reconfigure(encoding='utf-8')
 sys.stderr.reconfigure(encoding='utf-8')
 
