6e4395b200697fbfa2196b76f0ee2e14a32dceee
Jukka Jylänki <jujjyl@gmail.com>
2026-05-16 23:24:31 +0300

    test 3

--- tools/shared.py
+++ tools/shared.py
@@ -47,8 +47,8 @@ elif EMCC_LOGGING:
 logging.basicConfig(format='%(name)s:%(levelname)s: %(message)s', level=log_level)
 colored_logger.enable()
 
-sys.stdout.reconfigure(encoding='utf-8') ##     XXXXXXX
-sys.stderr.reconfigure(encoding='utf-8') ##     XXXXXXX
+#sys.stdout.reconfigure(encoding='utf-8') ##     XXXXXXX
+#sys.stderr.reconfigure(encoding='utf-8') ##     XXXXXXX
 
 import contextlib
 
